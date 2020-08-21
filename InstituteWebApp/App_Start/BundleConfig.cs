c74f3954804a1515d69a7f333783a24324b08056
Naveed Ahmad Hematmal <NaveedAhmadHematmal@gmail.com>
2020-08-21 15:30:21 +0430

    Microsoft jQuery CDN has been added

--- InstituteWebApp/App_Start/BundleConfig.cs
+++ InstituteWebApp/App_Start/BundleConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System.Web;
 using System.Web.Optimization;
+using System.Web.UI.WebControls;
 
 namespace WebApplication4
 {
@@ -11,6 +12,7 @@ namespace WebApplication4
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
 
+            bundles.Add(new ScriptBundle("~/Content/Scripts", "https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.5.1.js"));
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/Style.css"));
             bundles.Add(new ScriptBundle("~/Scripts/").Include(

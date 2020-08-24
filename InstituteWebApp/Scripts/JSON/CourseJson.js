ed424c4654c983ff81aa70bad45b35eeeebeda30
Naveed Ahmad Hematmal <NaveedAhmadHematmal@gmail.com>
2020-08-24 20:55:14 +0430

    Course Json separated from View

---
+++ InstituteWebApp/Scripts/JSON/CourseJson.js
@@ -0,0 +1,28 @@
+﻿var script = document.createElement('script');
+script.src = 'https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.5.1.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+        $(function () {
+            $('.button-style').click(function () {
+
+                var course = {
+                    CourseName: $(".CourseName").val(),
+                    TeacherName: $(".TeacherName").val(),
+                    Department: $(".Department").val()
+                }
+
+                $.ajax({
+                    type: "POST",
+                    contentType: "application/json; charset=utf-8",
+                    data: JSON.stringify(course),
+                    url: "/Faculty/GetCourseData",
+                    success: function (data) {
+                        alert("Data: " + data);
+                    },
+                    error: function (result) {
+                        alert("Problem occured" + result);
+                    }
+                });
+            })
+    });
